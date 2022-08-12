630257527a918f69afc5a86d0bf004e13ffd2bf4
delannushka <anna_delyan@mail.ru>
2022-08-12 16:26:46 +0300

    Задание 1.20. Внесла исправления

--- classes.php
+++ classes.php
@@ -9,65 +9,75 @@ class Task {
     //действия над заданиями
     //со стороны заказчика новое/в работе
     const ACTION_CANCEL = 'cancel';
-    const ACTION_GET_DONE = 'get_done';
+    const ACTION_GET_DONE = 'get done';
     //со стороны исполнителя новое/в работе
     const ACTION_RESPOND = 'respond';
     const ACTION_REFUSE = 'refuse';
 
-    public $idCustomer = null;
-    public $idExecutor = null;
+    public int $idCustomer;
+    public int $idExecutor;
 
     //конструктор для получения значений ID исполнителя и ID заказчика
-    public function __construct($idCustomer, $idExecutor)
+    public function __construct(int $idCustomer, int $idExecutor)
     {
         $this->idCustomer = $idCustomer;
         $this->idExecutor = $idExecutor;
     }
 
     //метод для возврата «карты» статусов
-    public function getStatusMap() {
-        return [
+    public function getStatusMap(): array
+    {
+        return array([
             self::STATUS_NEW => 'Новое',
             self::STATUS_CANCELLED => 'Отменено',
             self::STATUS_AT_WORK => 'В работе',
             self::STATUS_DONE => 'Выполнено',
             self::STATUS_FAILED => 'Провалено'
-        ];
+        ]);
     }
 
     //метод для возврата «карты» действий
-    public function getActionMap() {
-        return [
+    public function getActionMap(): array
+    {
+        return array([
           self::ACTION_CANCEL => 'Отменить',
           self::ACTION_GET_DONE => 'Выполнено',
           self::ACTION_RESPOND => 'Откликнуться',
           self::ACTION_REFUSE => 'Отказаться'
-        ];
+        ]);
     }
 
     //метод для получения доступных действий для указанного статуса
-    public function getAvailableActions($status) {
-        if ($status === self::STATUS_NEW) {
-            return [self::ACTION_CANCEL, self::ACTION_RESPOND];
+    public function getAvailableActions(string $status, int $userCurrentId): ?string
+    {
+        if ($status === self::STATUS_NEW && $userCurrentId === $this->idCustomer) {
+            return self::ACTION_CANCEL;
         }
-        if ($status === self::STATUS_AT_WORK) {
-            return [self::ACTION_GET_DONE, self::ACTION_REFUSE];
+        if ($status === self::STATUS_NEW && $userCurrentId === $this->idExecutor) {
+            return self::ACTION_RESPOND;
+        }
+        if ($status === self::STATUS_AT_WORK && $userCurrentId === $this->idCustomer) {
+            return self::ACTION_GET_DONE;
+        }
+        if ($status === self::STATUS_AT_WORK && $userCurrentId === $this->idExecutor) {
+            return self::ACTION_REFUSE;
         }
         return null;
     }
 
     //метод для получения статуса, в которой он перейдёт после выполнения указанного действия
-    public function getCurrentStatus($action) {
-        if ($action === self::ACTION_CANCEL) {
+    public function getNextStatus(string $action, string $currentStatus, int $userCurrentId): ?string
+    {
+        if ($action === self::ACTION_CANCEL && $currentStatus === self::STATUS_NEW && $userCurrentId === $this->idCustomer) {
             return self::STATUS_CANCELLED;
         }
-        if ($action === self::ACTION_GET_DONE) {
-            return self::STATUS_DONE;
-        }
-        if ($action === self::ACTION_RESPOND) {
+        if ($action === self::ACTION_RESPOND && $currentStatus === self::STATUS_NEW && $userCurrentId === $this->idExecutor) {
             return self::STATUS_AT_WORK;
         }
-        if ($action === self::ACTION_REFUSE) {
+        if ($action === self::ACTION_GET_DONE && $currentStatus === self::STATUS_AT_WORK && $userCurrentId === $this->idCustomer) {
+            return self::STATUS_DONE;
+        }
+        if ($action === self::ACTION_REFUSE && $currentStatus === self::STATUS_AT_WORK && $userCurrentId === $this->idExecutor) {
             return self::STATUS_FAILED;
         }
         return null;
--- test.php
+++ test.php
@@ -7,13 +7,22 @@ $mapStatus = $newTask->getStatusMap();
 
 $statusNew = 'new';
 $statusDone = 'done';
-$availableActionsNew = $newTask->getAvailableActions($statusNew);
-$availableActionsDone = $newTask->getAvailableActions($statusDone);
+$availableActionsNewCustomer = $newTask->getAvailableActions($statusNew, 4);
+$availableActionsNewExecutor = $newTask->getAvailableActions($statusNew, 6);
 
-$action = 'cancel';
-$currentStatus = $newTask->getCurrentStatus($action);
+$availableActionsDoneCustomer = $newTask->getAvailableActions($statusDone, 6);
+$availableActionsDoneExecutor = $newTask->getAvailableActions($statusDone, 4);
 
-var_dump($mapAction, $mapStatus, $availableActionsNew, $availableActionsDone, $currentStatus);
+var_dump($availableActionsNewCustomer, $availableActionsNewExecutor, $availableActionsDoneCustomer, $availableActionsDoneExecutor);
 
 $idCustomer = $newTask->idCustomer;
 var_dump($idCustomer);
+
+$nextStatus = $newTask->getNextStatus('cancel', 'new', 4);
+var_dump($nextStatus);
+$nextStatus = $newTask->getNextStatus('respond', 'new', 6);
+var_dump($nextStatus);
+$nextStatus = $newTask->getNextStatus('get done', 'work', 4);
+var_dump($nextStatus);
+$nextStatus = $newTask->getNextStatus('refuse', 'work', 6);
+var_dump($nextStatus);
