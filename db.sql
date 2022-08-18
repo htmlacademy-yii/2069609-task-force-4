4759c06cb46aff153cb08eb168388670ce3569d4
delannushka <anna_delyan@mail.ru>
2022-08-18 22:45:05 +0300

    Внесла правки Задание 2.6.

--- db.sql
+++ db.sql
@@ -13,7 +13,6 @@ CREATE TABLE user (
   role enum('executor', 'customer') NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
-  /* сохраняется в формате 9253266302 */
   phone VARCHAR(32),
   telegram VARCHAR(255),
   city_id INT NOT NULL,
@@ -44,30 +43,30 @@ CREATE TABLE task (
   date_of_publication TIMESTAMP DEFAULT NOW(),
   description VARCHAR(255) NOT NULL,
   date_of_execution DATE,
-  id_category INT NOT NULL,
+  category_id INT NOT NULL,
   files VARCHAR(255),
   FOREIGN KEY (city_id) REFERENCES city (id),
   FOREIGN KEY (user_id) REFERENCES user (id),
-  FOREIGN KEY (id_category) REFERENCES category (id)
+  FOREIGN KEY (category_id) REFERENCES category (id)
 );
 /* отклики */
 CREATE TABLE response (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  id_user INT NOT NULL,
-  id_task INT NOT NULL,
+  user_id INT NOT NULL,
+  task_id INT NOT NULL,
   price INT UNSIGNED NOT NULL,
   comment VARCHAR(255) NOT NULL,
   score TINYINT NOT NULL,
   feedback VARCHAR(255) NOT NULL,
-  FOREIGN KEY (id_user) REFERENCES user (id),
-  FOREIGN KEY (id_task) REFERENCES task (id)
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (task_id) REFERENCES task (id)
 );
 /* таблица связывающая исполнителей и категории */
 CREATE TABLE executor_category (
-  id_category INT AUTO_INCREMENT PRIMARY KEY,
-  id_user INT NOT NULL,
-  FOREIGN KEY (id_user) REFERENCES user (id),
-  FOREIGN KEY (id_category) REFERENCES category (id)
+  category_id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (category_id) REFERENCES category (id)
 );
 
 
