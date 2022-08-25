678154f90030c84861cd5d4193081c0326ab37cb
delannushka <anna_delyan@mail.ru>
2022-08-25 15:01:23 +0300

    Выполнила задание 3.8. Исключительный тип

--- src/Task.php
+++ src/Task.php
@@ -1,6 +1,8 @@
 <?php
 namespace Delta\TaskForce;
 
+use Delta\TaskForce\ex\IncomingDataException;
+
 class Task {
     //статусы заданий
     const STATUS_NEW = 'new';
@@ -20,6 +22,7 @@ class Task {
     public int $idExecutor;
 
     //конструктор для получения значений ID исполнителя и ID заказчика
+
     public function __construct(int $idCustomer, int $idExecutor)
     {
         $this->idCustomer = $idCustomer;
@@ -50,26 +53,49 @@ class Task {
     }
 
     //метод для получения доступных действий для указанного статуса
+
+    /**
+     * @throws IncomingDataException
+     */
     public function getAvailableActions(string $status, int $userCurrentId): ?Action
     {
-        if ($status === self::STATUS_NEW && CancelAction::isAvailable($userCurrentId, $this->idCustomer, $this->idExecutor)) {
+        $isCanselActionAvailable = CancelAction::isAvailable($userCurrentId, $this->idCustomer, $this->idExecutor);
+        $isRespondActionAvailable = RespondAction::isAvailable($userCurrentId, $this->idCustomer, $this->idExecutor);
+        $isGetDoneActionAvailable = GetDoneAction::isAvailable($userCurrentId, $this->idCustomer, $this->idExecutor);
+        $isRefuseActionAvailable = RefuseAction::isAvailable($userCurrentId, $this->idCustomer, $this->idExecutor);
+
+        if ($status !== self::STATUS_NEW) {
+            if ($status !== self::STATUS_AT_WORK) {
+                throw new IncomingDataException("Для текущего статуса нет доступных действий");
+            }
+        }
+
+        if ($status === self::STATUS_NEW && $isCanselActionAvailable) {
             return new CancelAction();
         }
-        if ($status === self::STATUS_NEW && RespondAction::isAvailable($userCurrentId, $this->idCustomer, $this->idExecutor)) {
-            return new RespondAction();
-        }
-        if ($status === self::STATUS_AT_WORK && GetDoneAction::isAvailable($userCurrentId, $this->idCustomer, $this->idExecutor)) {
+        if ($status === self::STATUS_AT_WORK && $isGetDoneActionAvailable) {
             return new GetDoneAction();
         }
-        if ($status === self::STATUS_AT_WORK && RefuseAction::isAvailable($userCurrentId, $this->idCustomer, $this->idExecutor)) {
+        if ($status === self::STATUS_NEW && $isRespondActionAvailable) {
+            return new RespondAction();
+        }
+        if ($status === self::STATUS_AT_WORK && $isRefuseActionAvailable) {
             return new RefuseAction();
         }
         return null;
     }
 
     //метод для получения статуса, в которой он перейдёт после выполнения указанного действия
+
+    /**
+     * @throws IncomingDataException
+     */
     public function getNextStatus(string $action, string $currentStatus, int $userCurrentId): ?string
     {
+        if (($action !== self::ACTION_CANCEL) && ($action !== self::ACTION_RESPOND) && ($action !== self::ACTION_GET_DONE) && ($action !== self::ACTION_REFUSE)
+        || ($currentStatus !== self::STATUS_NEW) && ($currentStatus === self::STATUS_AT_WORK)) {
+            throw new IncomingDataException("Вводимого действия/текущего статуса не существует");
+        }
         if ($action === self::ACTION_CANCEL && $currentStatus === self::STATUS_NEW && $userCurrentId === $this->idCustomer) {
             return self::STATUS_CANCELLED;
         }
---
+++ src/ex/IncomingDataException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Delta\TaskForce\ex;
+
+class IncomingDataException extends \Exception
+{
+
+}
