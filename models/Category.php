b109ac97ef179fce1da8a7213805ef4f44f5e456
delannushka <anna_delyan@mail.ru>
2022-09-11 17:47:41 +0300

    Выполнила задание 5.7. Армия моделей

---
+++ models/Category.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $icon
+ *
+ * @property ExecutorCategory[] $executorCategories
+ * @property Task[] $tasks
+ * @property User[] $users
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName(): string
+    {
+        return 'category';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules(): array
+    {
+        return [
+            [['name', 'icon'], 'required'],
+            [['name'], 'string', 'max' => 64],
+            [['icon'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels(): array
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'icon' => 'Icon',
+        ];
+    }
+
+    /**
+     * Gets query for [[ExecutorCategories]].
+     *
+     * @return \yii\db\ActiveQuery|ExecutorCategoryQuery
+     */
+    public function getExecutorCategories()
+    {
+        return $this->hasMany(ExecutorCategory::class, ['category_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Tasks]].
+     *
+     * @return \yii\db\ActiveQuery|TaskQuery
+     */
+    public function getTasks()
+    {
+        return $this->hasMany(Task::class, ['category_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Users]].
+     *
+     * @return \yii\db\ActiveQuery|UserQuery
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function getUsers()
+    {
+        return $this->hasMany(User::class, ['id' => 'user_id'])->viaTable('executor_category', ['category_id' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return CategoryQuery the active query used by this AR class.
+     */
+    public static function find(): CategoryQuery
+    {
+        return new CategoryQuery(get_called_class());
+    }
+}
